f47571cdc83c314af4857d499fc55e51c73d989e
ranjani <selvaranjani45@gmail.com>
2026-02-10 19:21:23 +0530

    added program

--- analysis.py
+++ analysis.py
@@ -1,1 +1,16 @@
-created analysis.py file created
+import pandas as pd
+
+df = pd.read_csv("student_data.csv")
+
+print("student data:")
+print(df)
+
+avg = df["marks"].mean()
+print("\nAverage Marks:",avg)
+
+top_students = df[df["marks"] > 80]
+print("\nTop Students:")
+print("top_students")
+
+top_students.to_csv("top_students.csv",index=False)
+print("\nFile saved: top_students.csv")
